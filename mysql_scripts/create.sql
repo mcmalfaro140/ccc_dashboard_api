3fa2711242904ff146d8154e285abff40f1d339e
AHorejsi <alex.horejsi59@gmail.com>
2019-10-28 13:55:27 -0700

    New MySQL database creation query

--- mysql_scripts/create.sql
+++ mysql_scripts/create.sql
@@ -9,3 +9,9 @@ CREATE TABLE IF NOT EXISTS Users (
 	Dashboard JSON,
 	PRIMARY KEY (UserId)
 );
+
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("AlexHorejsi", "alex.horejsi59@gmail.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("MisaelCorvera", "mcmalfaro140@gmail.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("YiWang", "superhotdogzz@gmail.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("ZacYou", "zacyou151@yahoo.com", NULL);
+INSERT INTO Users (Username, Email, Dashboard) VALUES ("JatDida", "didajateni@gmail.com", NULL);
