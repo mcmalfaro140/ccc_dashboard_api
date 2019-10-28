c25ff292163550981de5a195afbfe5327abaf2eb
AHorejsi <alex.horejsi59@gmail.com>
2019-10-28 13:55:27 -0700

    New MySQL database creation query

--- mysql_scripts/create.sql
+++ mysql_scripts/create.sql
@@ -11,8 +11,10 @@ CREATE TABLE IF NOT EXISTS Users (
 	PRIMARY KEY (UserId)
 );
 
+
 INSERT INTO Users (Username, Password, Email, Dashboard) VALUES ("AlexHorejsi","test1", "alex.horejsi59@gmail.com", "{}");
 INSERT INTO Users (Username, Password, Email, Dashboard) VALUES ("MisaelCorvera","test2", "mcmalfaro140@gmail.com", "{}");
 INSERT INTO Users (Username, Password, Email, Dashboard) VALUES ("YiWang","test3", "superhotdogzz@gmail.com", "{}");
 INSERT INTO Users (Username, Password, Email, Dashboard) VALUES ("ZacYou","test4", "zacyou151@yahoo.com", "{}");
 INSERT INTO Users (Username, Password, Email, Dashboard) VALUES ("JatDida","test5", "didajateni@gmail.com", "{}");
+
