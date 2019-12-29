6e13a9638050deb76ec3c0802f4d5c244ba1a31e
mcmalfaro140 <mcmalfaro@hotmail.com>
2019-12-28 19:18:14 -0800

    reading header

--- src/main/java/com/ccc/api/controller/HomeController.java
+++ src/main/java/com/ccc/api/controller/HomeController.java
@@ -1,9 +1,15 @@
 package com.ccc.api.controller;
 
 
+import java.util.Map;
+import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.RequestHeader;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 //import org.slf4j.MDC;
 
@@ -15,16 +21,28 @@ public class HomeController {
 
     @RequestMapping("/")
     public String hello() {
-  /*
-    	MDC.put("testKey", "testValue");
-        logger.error("An error message");
-        logger.warn("A warning message");
-        logger.info("An information message");
-        logger.info("Try this");
-        logger.debug("A debugging message");
-        logger.trace("A message for more detailed debugging");
-        MDC.remove("testKey");
-        */
-        return "Hello World in Spring Boot xxx";
+        return "Hello World in Spring Boot misael";
+    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @ResponseBody
+    public String getFoosBySimplePath(@RequestHeader Map<String, String> headers) {
+    	Boolean[] arr = {false}; 
+    	String[] user = {""};
+    	headers.forEach((key, value) -> {
+    		if(key.equals("user")) {
+    			user[0] = value;
+    		}
+        });
+    	
+    	return "User found return true you are "+ user[0];
+    	
+//    	if(arr[0]) {
+//    		return "User found return true you are "+ user[0];
+//    	}else {
+//    		return "User NOT found return true";
+//    	}
+    	
+        
     }
 }
