900e9fa9ec9a80b3240d996e259b2cfa870241bb
mcmalfaro140 <mcmalfaro@hotmail.com>
2019-12-30 19:13:23 -0800

    database connected

--- src/main/java/com/ccc/api/controller/HomeController.java
+++ src/main/java/com/ccc/api/controller/HomeController.java
@@ -11,13 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ccc.api.model.Users;
+import com.ccc.api.model.dao.UsersDao;
 //import org.slf4j.MDC;
+
 
 
 @RestController
 public class HomeController {
-
-//    Logger logger = LoggerFactory.getLogger(TestController.class);
+	
+	@Autowired
+    private UsersDao usersDao;
+	
+	 @RequestMapping("/Users")
+    public List<Users> users(ModelMap models) {
+        return usersDao.getUsers();
+    }
 
     @RequestMapping("/")
     public String hello() {
