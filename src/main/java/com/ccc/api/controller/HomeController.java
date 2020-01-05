7d100045c51fe7a4d783308d42740f3499a66449
mcmalfaro140 <mcmalfaro@hotmail.com>
2020-01-05 15:23:58 -0800

    added require fields for auth

--- src/main/java/com/ccc/api/controller/HomeController.java
+++ src/main/java/com/ccc/api/controller/HomeController.java
@@ -43,7 +43,7 @@ public class HomeController {
     }
     
     @CrossOrigin(origins = "http://localhost:3000")
-    @RequestMapping(path = "/login", produces = "application/json; charset=UTF-8")
+    @RequestMapping(path = "/authenticate", produces = "application/json; charset=UTF-8")
     @ResponseBody
     public HashMap<String, Object>  getFoosBySimplePath(@RequestBody Map<String, String> payload) {
     	String userName = "misael";
@@ -53,8 +53,13 @@ public class HomeController {
     	String testPass = payload.get("password");
     	
     	if(testUser.equals(userName) && testPass.equals(pass)) {
-    		response.put("User", userName);
-		    response.put("Dashboard", "none");
+    		response.put("id", "1");
+    		response.put("username", userName);
+    		response.put("firstName", "Misael");
+    		response.put("lastName", "Corvera");
+    		response.put("role", "Admin");
+    		response.put("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDb2RlcnRoZW1lIiwiaWF0IjoxNTU1NjgyNTc1LCJleHAiOjE1ODcyMTg1NzUsImF1ZCI6ImNvZGVydGhlbWVzLmNvbSIsInN1YiI6InRlc3QiLCJmaXJzdG5hbWUiOiJIeXBlciIsImxhc3RuYW1lIjoiVGVzdCIsIkVtYWlsIjoidGVzdEBoeXBlci5jb2RlcnRoZW1lcy5jb20iLCJSb2xlIjoiQWRtaW4ifQ.8qHJDbs5nw4FBTr3F8Xc1NJYOMSJmGnRma7pji0YwB4");
+		    response.put("Dashboard", "[{\"objectType\":\"graph\",\"graphSettings\":{\"type\":\"line\",\"realTime\":\"false\",\"metricName\":\"CPUUtilization\",\"nameSpace\":\"AWS/EC2\",\"chartName\":\"Test\",\"instanceId\":\"i-01e27ec0da2c4d296\",\"refreshRate\":\"\",\"period\":180},\"coordinates\":{\"x\":0,\"y\":1,\"w\":20,\"h\":19,\"minW\":6,\"minH\":9}}]");
     	}else {
     		response.put("error", "Username or password is incorrect");
     	}
