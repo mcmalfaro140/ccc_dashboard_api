ba7b312ecc53c43dec61ebfe138c58eb3807a8fc
superhotdogzz <superhotdogzz@gmail.com>
2020-01-27 15:28:51 -0800

    Frontend connection, retriving user data from database on demand

---
+++ src/main/java/com/ccc/api/controller/UsersRepository.java
@@ -0,0 +1,17 @@
+package com.ccc.api.controller;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.repository.query.QueryByExampleExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.ccc.api.model.Users;
+
+@Repository
+public interface UsersRepository extends JpaRepository<Users, Integer>,QueryByExampleExecutor<Users>  {
+	@Query("select u from Users u where u.username = :username")
+	  Users findByUsername(@Param("username") String username);
+	}
