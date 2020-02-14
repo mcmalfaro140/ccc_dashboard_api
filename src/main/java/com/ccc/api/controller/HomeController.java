e5d66c16deefb85446dbed2dfd5f0a524e679b03
superhotdogzz <superhotdogzz@gmail.com>
2020-02-14 11:52:32 -0800

    Http status return from auth controller

--- src/main/java/com/ccc/api/controller/HomeController.java
+++ src/main/java/com/ccc/api/controller/HomeController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,7 +51,7 @@ public class HomeController {
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(path = "/authenticate", produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public HashMap<String, Object>  getFoosBySimplePath(@RequestBody Map<String, String> payload) {
+    public ResponseEntity<HashMap<String, Object>>  getFoosBySimplePath(@RequestBody Map<String, String> payload) {
     	HashMap<String, Object> response = new HashMap<>();
     	String inUser = payload.get("username");
     	String inPass = payload.get("password");
@@ -61,43 +63,23 @@ public class HomeController {
     		{
     			response.put("id",target.getUserId().toString());
     	    	response.put("username", target.getUsername());
-    	    	response.put("Dashboard", target.getDashboard());
+    	    	//response.put("Dashboard", target.getDashboard());
     	    	response.put("role", "admin");
     	    	response.put("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDb2RlcnRoZW1lIiwiaWF0IjoxNTU1NjgyNTc1LCJleHAiOjE1ODcyMTg1NzUsImF1ZCI6ImNvZGVydGhlbWVzLmNvbSIsInN1YiI6InRlc3QiLCJmaXJzdG5hbWUiOiJIeXBlciIsImxhc3RuYW1lIjoiVGVzdCIsIkVtYWlsIjoidGVzdEBoeXBlci5jb2RlcnRoZW1lcy5jb20iLCJSb2xlIjoiQWRtaW4ifQ.8qHJDbs5nw4FBTr3F8Xc1NJYOMSJmGnRma7pji0YwB4");
     		    //response.put("Dashboard", "[{\"objectType\":\"graph\",\"graphSettings\":{\"type\":\"line\",\"realTime\":\"false\",\"metricName\":\"CPUUtilization\",\"nameSpace\":\"AWS/EC2\",\"chartName\":\"Test\",\"instanceId\":\"i-01e27ec0da2c4d296\",\"refreshRate\":\"\",\"period\":180},\"coordinates\":{\"x\":0,\"y\":1,\"w\":20,\"h\":19,\"minW\":6,\"minH\":9}}]");
     		}
     		else {
     			response.put("error","wrong password");
+    			return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.PRECONDITION_FAILED);
     		}
     	}else {
     	response.put("error", "user not found");
+    	return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.NOT_FOUND);
     	}
     	
 
-    	return response;
+    	return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.OK);
     }
     
-    
-    @CrossOrigin(origins = "http://localhost:3000")
-    @PutMapping(path = "/update", produces = "application/json; charset=UTF-8")
-    @ResponseBody
-    public HashMap <String, Object> updatemap(@RequestBody Map<String, String> payload)
-    {
-    	HashMap<String, Object> response = new HashMap<>();
-    	String inUser = payload.get("username");
-    	String dashBoard = payload.get("dashboard");
-    	Users target = usersRepository.findByUsername(inUser);
-    	
-    	if(target != null)
-    	{
-    		target.setDashboard(dashBoard);
-        	usersRepository.save(target);
-    	}
-    	else {
-    		response.put("error", "user not found");
-    	}
-		return response;
-    	
-    }
     
 }
