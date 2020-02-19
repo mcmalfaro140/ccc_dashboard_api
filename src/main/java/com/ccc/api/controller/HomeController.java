cd4b8fedca518406c00c406f8a50707227c2b34a
mcmalfaro140 <mcmalfaro@hotmail.com>
2020-02-18 18:52:51 -0800

    roll back

--- src/main/java/com/ccc/api/controller/HomeController.java
+++ src/main/java/com/ccc/api/controller/HomeController.java
@@ -51,7 +51,7 @@ public class HomeController {
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(path = "/authenticate", produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public ResponseEntity<HashMap<String, Object>>  getFoosBySimplePath(@RequestBody Map<String, String> payload) {
+    public HashMap<String , Object>  getFoosBySimplePath(@RequestBody Map<String, String> payload) {
     	HashMap<String, Object> response = new HashMap<>();
     	String inUser = payload.get("username");
     	String inPass = payload.get("password");
@@ -63,22 +63,23 @@ public class HomeController {
     		{
     			response.put("id",target.getUserId().toString());
     	    	response.put("username", target.getUsername());
+    	    	response.put("firstName", "Misael");
+    	    	response.put("lastName", "Alfaro");
     	    	//response.put("Dashboard", target.getDashboard());
     	    	response.put("role", "admin");
     	    	response.put("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDb2RlcnRoZW1lIiwiaWF0IjoxNTU1NjgyNTc1LCJleHAiOjE1ODcyMTg1NzUsImF1ZCI6ImNvZGVydGhlbWVzLmNvbSIsInN1YiI6InRlc3QiLCJmaXJzdG5hbWUiOiJIeXBlciIsImxhc3RuYW1lIjoiVGVzdCIsIkVtYWlsIjoidGVzdEBoeXBlci5jb2RlcnRoZW1lcy5jb20iLCJSb2xlIjoiQWRtaW4ifQ.8qHJDbs5nw4FBTr3F8Xc1NJYOMSJmGnRma7pji0YwB4");
     		    //response.put("Dashboard", "[{\"objectType\":\"graph\",\"graphSettings\":{\"type\":\"line\",\"realTime\":\"false\",\"metricName\":\"CPUUtilization\",\"nameSpace\":\"AWS/EC2\",\"chartName\":\"Test\",\"instanceId\":\"i-01e27ec0da2c4d296\",\"refreshRate\":\"\",\"period\":180},\"coordinates\":{\"x\":0,\"y\":1,\"w\":20,\"h\":19,\"minW\":6,\"minH\":9}}]");
+ 
     		}
     		else {
-    			response.put("error","wrong password");
-    			return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.PRECONDITION_FAILED);
+    			response.put("passError","wrong password");
+    			return (response);
     		}
     	}else {
-    	response.put("error", "user not found");
-    	return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.NOT_FOUND);
+	    	response.put("userError", "user not found");
+	    	return (response);
     	}
-    	
-
-    	return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.OK);
+    	return (response);
     }
     
     @CrossOrigin(origins = "http://localhost:3000")
