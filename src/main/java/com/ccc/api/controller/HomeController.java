3144c2eb1223a4702f22c43524f10b6da10a1352
superhotdogzz <superhotdogzz@gmail.com>
2020-02-21 12:30:38 -0800

    changing the auth route error check

--- src/main/java/com/ccc/api/controller/HomeController.java
+++ src/main/java/com/ccc/api/controller/HomeController.java
@@ -51,10 +51,11 @@ public class HomeController {
     @CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(path = "/authenticate", produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public ResponseEntity<HashMap<String, Object>>  getFoosBySimplePath(@RequestBody Map<String, String> payload) {
+    public HashMap<String, Object>  getFoosBySimplePath(@RequestBody Map<String, String> payload) {
     	HashMap<String, Object> response = new HashMap<>();
     	String inUser = payload.get("username");
     	String inPass = payload.get("password");
+    	String dash = payload.get("dash");
     	
     	Users target = usersRepository.findByUsername(inUser);
     	if (target != null)
@@ -63,22 +64,17 @@ public class HomeController {
     		{
     			response.put("id",target.getUserId().toString());
     	    	response.put("username", target.getUsername());
-    	    	//response.put("Dashboard", target.getDashboard());
-    	    	response.put("role", "admin");
+    	    	response.put("dashboard", target.getDashboard());
     	    	response.put("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDb2RlcnRoZW1lIiwiaWF0IjoxNTU1NjgyNTc1LCJleHAiOjE1ODcyMTg1NzUsImF1ZCI6ImNvZGVydGhlbWVzLmNvbSIsInN1YiI6InRlc3QiLCJmaXJzdG5hbWUiOiJIeXBlciIsImxhc3RuYW1lIjoiVGVzdCIsIkVtYWlsIjoidGVzdEBoeXBlci5jb2RlcnRoZW1lcy5jb20iLCJSb2xlIjoiQWRtaW4ifQ.8qHJDbs5nw4FBTr3F8Xc1NJYOMSJmGnRma7pji0YwB4");
-    		    //response.put("Dashboard", "[{\"objectType\":\"graph\",\"graphSettings\":{\"type\":\"line\",\"realTime\":\"false\",\"metricName\":\"CPUUtilization\",\"nameSpace\":\"AWS/EC2\",\"chartName\":\"Test\",\"instanceId\":\"i-01e27ec0da2c4d296\",\"refreshRate\":\"\",\"period\":180},\"coordinates\":{\"x\":0,\"y\":1,\"w\":20,\"h\":19,\"minW\":6,\"minH\":9}}]");
     		}
     		else {
-    			response.put("error","wrong password");
-    			return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.PRECONDITION_FAILED);
+    			response.put("error","Incorrect Password.");
     		}
     	}else {
-    	response.put("error", "user not found");
-    	return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.NOT_FOUND);
+    	response.put("error", "User Not Found.");
     	}
+    	return response;
     	
-
-    	return new ResponseEntity<HashMap<String, Object>> (response,HttpStatus.OK);
     }
     
     @CrossOrigin(origins = "http://localhost:3000")
