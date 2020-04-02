9cb8a78edfa4511a8bd50aa21fc90316ebe3d51c
AHorejsi <alex.horejsi59@gmail.com>
2020-04-02 08:39:54 -0700

    Updating the way UserController works to abide by Spring JPA best practices

--- src/main/java/com/ccc/api/ApiApplication.java
+++ src/main/java/com/ccc/api/ApiApplication.java
@@ -3,7 +3,6 @@ package com.ccc.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ApiApplication {
 	public static void main(String[] args) {
--- src/main/java/com/ccc/api/repository/LogAlarmRepository.java
+++ src/main/java/com/ccc/api/repository/LogAlarmRepository.java
@@ -1,21 +1,10 @@
 package com.ccc.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ccc.api.model.LogAlarm;
 
 @Repository
 public interface LogAlarmRepository extends JpaRepository<LogAlarm, Long> {
-	@Query(
-		value="INSERT INTO LogAlarms (LogLevelCriteriaId, AlarmName, Comparison) VALUES (:logLevelCriteriaId, :alarmName, :keywordRelationship)",
-		nativeQuery=true
-	)
-	public void insert(
-			@Param(value="logLevelCriteriaId") Long logLevelCriteriaId,
-			@Param(value="alarmName") String alarmName,
-			@Param(value="keywordRelationship") String keywordRelationship
-	);
 }
