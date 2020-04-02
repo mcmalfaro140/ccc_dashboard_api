42abcc125cfe639f6a7f4bce3e0a7b0799c8351e
AHorejsi <alex.horejsi59@gmail.com>
2020-04-02 09:06:00 -0700

    Stuff

--- src/main/java/com/ccc/api/controller/UserController.java
+++ src/main/java/com/ccc/api/controller/UserController.java
@@ -9,19 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import com.ccc.api.service.UserService;
+import com.ccc.api.model.User;
+import com.ccc.api.repository.UserRepository;
+import com.ccc.api.util.JwtUtils;
 
 @RestController
-@Transactional
 public class UserController {
 	@Autowired
-	private UserService userService;
+	private UserRepository userRepository;
+	
+	@Autowired
+	private JwtUtils jwtutils;
 	
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
@@ -34,28 +39,76 @@ public class UserController {
 			}
 		};
 	}
+
+
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello, BACKEND server for the CCC Dashboard Project";
+    }
     
     @RequestMapping(path = "/authenticate", produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public Map<String, Object> getUsersBySimplePath(@RequestBody Map<String, String> payload) {
-    	String username = payload.get("username");
-    	String password = payload.get("password");
+    public HashMap<String , Object> getUsersBySimplePath(@RequestBody Map<String, String> payload) {
+    	HashMap<String, Object> response = new HashMap<>();
+    	String inUser = payload.get("username");
+    	String inPass = payload.get("password");
+    	User target = userRepository.findByUsername(inUser);
     	
-    	return this.userService.getByUsernameAndPassword(username, password);
+    	if (target != null) {
+    		if (inPass.contentEquals(target.getPassword())) {
+    			String jwt = jwtutils.toToken(target);
+    	    	response.put("username", target.getUsername());
+    	    	response.put("token", jwt);
+    		}
+    		else {
+    			response.put("error","Incorrect Password.");
+    		}
+    	} 
+    	else {
+    		response.put("error", "User Not Found.");
+    	}
+    	
+    	return response;
     }
     
     @PostMapping(path = "/update", produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public Map<String, Object> updateDashboard(@RequestBody Map<String, String> payload) {
+    public HashMap<String, Object> updatemap(@RequestBody Map<String, String> payload) {
+    	HashMap<String, Object> response = new HashMap<>();
     	String token = payload.get("token");
-    	String dashboard = payload.get("dashboard");
+    	String dashBoard = payload.get("dashboard");
+
+    	User toUser = jwtutils.toUser(token);
     	
-    	return this.userService.updateDashboard(token, dashboard);
+    	if (toUser == null) {
+    		response.put("error", "token is having issue");
+    		return response;
+    	}
+    	
+    	String inUser = toUser.getUsername();
+    	User target = userRepository.findByUsername(inUser);
+    	
+    	if (target != null) {
+    		target.setDashboard(dashBoard);
+    		userRepository.save(target);
+    	}
+    	else {
+    		response.put("error", "user not found");
+    	}
+    	
+		return response;
     }
     
     @RequestMapping(path = "/get_dashboard" , produces = "application/json; charset=UTF-8")
     @ResponseBody
-    public Map<String, Object> getDashboard(@RequestHeader("Authorization") String token) {
-    	return this.userService.getDashboard(token);
+    public HashMap<String, Object> getdashboard(@RequestHeader("Authorization") String token) {
+    	User toUsers = jwtutils.toUser(token);
+    	String inUser = toUsers.getUsername();
+    	User target = userRepository.findByUsername(inUser);
+    	
+    	HashMap<String, Object> response = new HashMap<>();
+    	response.put("dashboard", target.getDashboard());
+    	
+    	return response;
     }
 }
