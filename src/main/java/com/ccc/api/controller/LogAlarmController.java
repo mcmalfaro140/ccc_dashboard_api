4edc4c813afbb2dc1517f506cc741a4601ab33c8
AHorejsi <alex.horejsi59@gmail.com>
2020-04-07 14:37:13 -0700

    Removed unnecessary logger

--- src/main/java/com/ccc/api/controller/LogAlarmController.java
+++ src/main/java/com/ccc/api/controller/LogAlarmController.java
@@ -8,8 +8,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,9 +36,7 @@ import com.ccc.api.util.JwtUtils;
 
 @RestController
 @Transactional
-public class LogAlarmController {
-	Logger logger = LoggerFactory.getLogger(LogAlarmController.class);
-	
+public class LogAlarmController {	
 	@Autowired
 	private LogAlarmRepository logAlarmRepo;
 	
