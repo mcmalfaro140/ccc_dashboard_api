3fde3aca8c6e08ce7f4b119b3f63c89f5364cd71
mcmalfaro140 <mcmalfaro@hotmail.com>
2020-04-07 18:14:26 -0700

    testing state 2.0

--- src/main/java/com/ccc/api/controller/LogAlarmController.java
+++ src/main/java/com/ccc/api/controller/LogAlarmController.java
@@ -10,11 +10,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,6 +56,8 @@ public class LogAlarmController {
 	@Autowired
 	private JwtUtils jwtUtils;
 	
+	@RequestMapping(path="/getLogAlarms", produces="application/json; charset=UTF-8")
+	@ResponseBody
 	public Map<String, Object> getLogAlarms(@RequestHeader(name="Authorization") String token) {
 		Map<String, Object> response = new HashMap<String, Object>();
 		List<LogAlarm> allLogAlarms = this.logAlarmRepo.findAll();
